033a99b3a7fbf7a1d4e3f3e298a62a47b586eae9
Vanessa <zdperfectx@gmail.com>
2023-10-22 17:39:18 -0500

    organizing folders and files. created home folder with deck and decklist within

---
+++ src/home/Deck.js
@@ -0,0 +1,11 @@
+
+
+// create a deck
+// each deck shows: 
+// the deck name,
+// the number of cards,
+// and the View, Study, and Delete button
+// clicking View button brings user to the Deck screen 
+// clicking Study button brings user to Study screen
+// clicking Delete button shows warning message before deleting deck 
+// // // delete handler needed 
---
+++ src/home/DeckList.js
@@ -0,0 +1,5 @@
+
+
+// path is '/'
+// top of list of deck is a create deck button that brings user to Create Deck screen
+// map through list of decks 
