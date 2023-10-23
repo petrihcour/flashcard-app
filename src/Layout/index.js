f93ee99b5e691fc1f152eb76e16927aba4f9490f
Vanessa <zdperfectx@gmail.com>
2023-10-23 14:22:33 -0500

    mapping out parent component index.js in layout and setting up routes before writing specific code for components

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import Header from "./Header";
-import DeckList from "../home/DeckList"
+import DeckList from "../home/DeckList";
 import NotFound from "./NotFound";
 
 function Layout() {
@@ -15,8 +15,32 @@ function Layout() {
           <Route>
             <DeckList />
           </Route>
+          <Route>
+            <StudyCard />
+          </Route>
+          <Route>
+            <DeckScreen />
+          </Route>
+          <Route>
+            <CreateDeck />
+          </Route>
+          <Route>
+            <EditDeck />
+          </Route>
+          <Route>
+            <AddCard />
+          </Route>
+          <Route>
+            <EditCard />
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+          <Route>
+            <NotEnough />
+          </Route>
         </Switch>
-        <NotFound />
+        <NavHome />
       </div>
     </>
   );
