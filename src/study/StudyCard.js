b782fe3e3be01a042c53fbdccd9ed335f07cf6e6
Vanessa <zdperfectx@gmail.com>
2023-10-25 17:33:34 -0500

    completed studycard component addeded conditional statements for clickhandling

--- src/study/StudyCard.js
+++ src/study/StudyCard.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, useHistory, useRouteMatch } from "react-router-dom";
 import NavHome from "../home/NavHome";
 import NotFound from "../Layout/NotFound";
 
@@ -20,6 +20,9 @@ function StudyCard({ decks }) {
   const [cardIndex, setCardIndex] = useState(0);
   const [showFront, setShowFront] = useState(true);
 
+  const history = useHistory();
+  const { url } = useRouteMatch();
+
   const { deckId } = useParams();
   const deck = decks.find((deck) => deck.id === Number(deckId));
   if (!deck) {
@@ -35,37 +38,71 @@ function StudyCard({ decks }) {
     setShowFront(!showFront);
   };
 
+  const clickHandler = () => {
+    if (cardIndex < totalCards - 1) {
+      setCardIndex(cardIndex + 1);
+      setShowFront(true);
+    } else {
+      const restartConfirmed = window.confirm(
+        `Restart cards? \n \nClick 'OK' to restart or 'Cancel' to return to the home page.`
+      );
+      if (restartConfirmed) {
+        setCardIndex(0);
+        setShowFront(true);
+      } else {
+        history.push("/");
+      }
+    }
+  };
+
+  if (totalCards > 2) {
+    return (
+      <div>
+        <NavHome deck={deck.name} heading="Study" />
+        <h1>Study: {deck.name}</h1>
+
+        <div className="card mx-auto">
+          <div className="card-body">
+            <h5 className="card-title">
+              Card {cardIndex + 1} of {totalCards}{" "}
+            </h5>
+
+            <p className="card-text">{showFront ? card.front : card.back}</p>
+
+            <div style={{ display: "flex", justifyContent: "start" }}>
+              <button
+                className="btn btn-secondary mr-2"
+                type="button"
+                onClick={flipCard}
+              >
+                Flip
+              </button>
+              <button
+                className="btn btn-primary"
+                type="button"
+                style={{ display: showFront ? "none" : "block" }}
+                onClick={clickHandler}
+              >
+                Next
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
   return (
     <div>
       <NavHome deck={deck.name} heading="Study" />
       <h1>Study: {deck.name}</h1>
-
-      <div className="card mx-auto">
-        <div className="card-body">
-          <h5 className="card-title">
-            Card {cardIndex + 1} of {totalCards}{" "}
-          </h5>
-
-          <p className="card-text">{showFront ? card.front : card.back}</p>
-
-          <div style={{ display: "flex", justifyContent: "start" }}>
-            <button
-              className="btn btn-secondary mr-2"
-              type="button"
-              onClick={flipCard}
-            >
-              Flip
-            </button>
-            <button
-              className="btn btn-primary"
-              type="button"
-              style={{ display: showFront ? "none" : "block" }}
-            >
-              Next
-            </button>
-          </div>
-        </div>
-      </div>
+      <h2>Not enough cards.</h2>
+      <p>
+        You need at least 3 cards to study. There are {totalCards} cards in this
+        deck.
+      </p>
+      <Link to={`${url}/cards/new`} className="btn btn-primary">
+          <i className="bi bi-plus"></i> Add Cards
+     </Link>
     </div>
   );
 }
